e7baeeb25ed20581d4e3e6b1d455c7b3fd4bce11
jjunhaa0211 <goodjunha@gmail.com>
2025-06-09 09:41:57 +0900

    auto commit via pipeline

--- OJT2025061HomeWork/hello.c
+++ OJT2025061HomeWork/hello.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 int main(void) {
-    int myVariable = 0;        // ← 카멜표기 의도적 삽입
-    printf("Hello World! %d\n", myVariable);
+    printf("Hello World! %d\n");
     return 0;
 }
