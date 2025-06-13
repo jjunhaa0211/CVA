7ad4d28182f1ff6d2c0045b7a9f96a88b514feb9
jjunhaa0211 <goodjunha@gmail.com>
2025-06-13 18:00:31 +0900

    20250530

--- OJT202505302/main.c
+++ OJT202505302/main.c
@@ -6,9 +6,19 @@
 //
 
 #include <stdio.h>
+#include <stdlib.h>
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    printf("Hello, World!\n");
+int main(void)
+{
+    FILE *fp;
+
+    if ((fp = fopen("test.txt", "a")) == NULL) {
+        fprintf(stderr, "test.txt 파일을 열 수 없습니다.\n");
+        return 1;
+    }
+
+    fprintf(fp, "\n나는 하이지노의 제조DX팀에서 일하고 있습니다.");
+
+    fclose(fp);
     return 0;
 }
